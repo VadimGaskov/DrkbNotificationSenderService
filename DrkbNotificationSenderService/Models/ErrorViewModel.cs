1bce287357cda0abda7a0266b979eb1e60fca6b7
gaskov.vadim <gaskov.vadim@mail.ru>
2025-08-24 12:54:48 +0800

    init

---
+++ DrkbNotificationSenderService/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DrkbNotificationSenderService.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
